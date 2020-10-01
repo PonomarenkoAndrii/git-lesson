0dc2db2db1a449abfc3a3d8dfce552b417fae399
dn301293paa1 <dn301293paa1@gmail.com>
2020-10-01 23:56:06 +0300

    added new file

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS8H1u4SN2GP7eFhBmrAB--wA8RbmuduTCqqg&usqp=CAU" alt="fkmn" />
+        </header>
+    )
+}
+
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://www.worcester.gov.uk/images/easyblog_shared/2019/b2ap3_large_Football---carousel.jpg" alt="" />
+            </div>
+            <div>
+                ava + descriptions
+        </div>
+            <div>
+                My post
+          <div>
+                    new post
+          </div>
+                <div>
+                    <div>
+                        post 1
+            </div>
+                    <div>
+                        post 2
+            </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Profile
